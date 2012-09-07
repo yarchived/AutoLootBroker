44d06d51579ae5974c5c906765335f623199ad47
yaroot <yaroot@gmail.com>
2012-09-07 13:37:13 +0800

    .

--- toggle.lua
+++ toggle.lua
@@ -1,24 +1,38 @@
-local dataobj = LibStub('LibDataBroker-1.1'):NewDataObject('AutoLootBroker', {type = 'launcher',text = 'Loot: ...'})
+
+local dataobj = LibStub('LibDataBroker-1.1'):NewDataObject('AutoLootBroker', {
+    type = 'launcher',
+    icon = 'Interface\\Icons\\INV_Misc_Bag_07_Green',
+    text = 'Loot: ...'
+})
+
 local f = CreateFrame'Frame'
 local ON = 'Loot: |cff00ff00ON|r'
 local OFF = 'Loot: |cffff0000OFF|r'
-local already_in_group = false
 
-local function toggleAutoLoot(switch)
+local function setState(switch)
     if(switch) then
-        _G.SetCVar('autoLootDefault', '1')
         dataobj.text = ON
         dataobj.icon = 'Interface\\Icons\\INV_Misc_Bag_07_Green'
     else
-        _G.SetCVar('autoLootDefault', '0')
         dataobj.text = OFF
         dataobj.icon = 'Interface\\Icons\\INV_Misc_Bag_07_Red'
     end
 end
 
-local function Update()
+local function toggleAutoLoot(switch)
+    if(switch) then
+        SetCVar('autoLootDefault', '1')
+    else
+        SetCVar('autoLootDefault', '0')
+    end
+    return setState(switch)
 end
 
+local function isAutoLootOn()
+    return _G.GetCVar('autoLootDefault') == '1' 
+end
+
+local already_in_group = false
 f.GROUP_ROSTER_UPDATE = function()
     local new_state = IsInGroup()
     if(new_state ~= already_in_group) then
@@ -28,24 +42,24 @@ f.GROUP_ROSTER_UPDATE = function()
 end
 
 function f:PLAYER_LOGIN()
+    setState(isAutoLootOn())
     self.PLAYER_LOGIN = nil
     f:RegisterEvent'GROUP_ROSTER_UPDATE'
     return self:GROUP_ROSTER_UPDATE()
 end
 
 function dataobj.OnClick()
-    if _G.GetCVar('autoLootDefault') == '0' then
-        TurnOn()
-    elseif _G.GetCVar('autoLootDefault') == '1' then
-        TurnOFF()
+    local isOn = isAutoLootOn()
+    if(isOn) then
+        toggleAutoLoot(false)
+    else
+        toggleAutoLoot(true)
     end
 end
 
 function dataobj.OnTooltipShow(tooltip)
-    if not tooltip or not tooltip.AddLine then return end
-
     local status
-    if _G.GetCVar('autoLootDefault') == '1' then
+    if(isAutoLootOn()) then
         status = true
     end
     tooltip:AddDoubleLine('|cffff8800AutoLootToggle|r', status and '|cff00ff00ON|r' or '|cffff0000OFF|r')
@@ -53,5 +67,4 @@ end
 
 f:RegisterEvent'PLAYER_LOGIN'
 f:SetScript('OnEvent', function(self, event, ...) self[event](self, event, ...) end)
-
 
